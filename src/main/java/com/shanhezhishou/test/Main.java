3d5d2a9ea85ce6a06fe46949d3fb49ad4cb2cca1
山河执手 <2207297848@qq.com>
2022-01-14 14:18:50 +0800

    second commit

--- src/main/java/com/shanhezhishou/test/Main.java
+++ src/main/java/com/shanhezhishou/test/Main.java
@@ -3,5 +3,7 @@ package com.shanhezhishou.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("你好git");
+        System.out.println("你好git2");
+
     }
 }

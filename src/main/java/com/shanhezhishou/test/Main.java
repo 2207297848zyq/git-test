fb552976d8aebd68edce52e038f10fc1a42e56a8
山河执手 <2207297848@qq.com>
2022-01-14 15:01:41 +0800

    master commit

--- src/main/java/com/shanhezhishou/test/Main.java
+++ src/main/java/com/shanhezhishou/test/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("你好git2");
         System.out.println("你好git3");
         System.out.println("hot-fix-test");
+        System.out.println("冲突测试");
     }
 }
